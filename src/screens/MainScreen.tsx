4e276b35decf926f14c1e8fe428aca4994ff108d
Ji-hoon Lee <anteater1056@gmail.com>
2023-12-29 20:38:35 +0900

    layout: 1st draft 레이아웃
    강렬한 원색이 컨셉

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,15 +1,23 @@
-import TimerArea, { TimerAreaProp } from '@/components/TimerArea';
+import TimerArea, { TimerStatusType } from '@/components/TimerArea';
+import { useState } from 'react';
 
 /**
  * MainScreen은 TimerArea를 포함하고 있다.
  * MainScreen은 현재 화면에 표시할 Timer의 정보를 조작한다.
  */
 function MainScreen() {
-  const timerProp: TimerAreaProp = { type: 'pomodoro' };
+  const [timerStatus, setTimerStatus] = useState<TimerStatusType>('pomodoro');
 
   return (
     <div id="app-main-screen" className="h-full w-full">
-      <TimerArea {...timerProp} />
+      <TimerArea
+        type={timerStatus}
+        onStart={() => {
+          if (timerStatus === 'long-break') setTimerStatus('pomodoro');
+          else if (timerStatus === 'short-break') setTimerStatus('long-break');
+          else setTimerStatus('short-break');
+        }}
+      />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ export default {
     extend: {
       colors: {
         domadoRed: '#f14d30',
-        domadoGreen: '#3eae3c',
+        domadoGreen: '#2e922c',
         domadoSkyTop: '#188bff',
         domadoSkyBottom: '#54d7ff',
         /** use this with random alpha */
         domadoCloud: '#faf2ef',
+      },
+      fontFamily: {
+        santokki: ['HS-Regular'],
       },
     },
   },
