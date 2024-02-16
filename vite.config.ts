7559dd1bb2b03c11124304bc7540bfc479a7de45
Ji-hoon Lee <anteater1056@gmail.com>
2024-02-16 12:02:28 +0900

    chore: localhost 환경 HTTPS 적용
    Screen Wake Lock API는 HTTPS 환경에서만 제공되므로 HTTPS 환경 테스트를 위한 패키지 설치 및 설정 적용
    
    다만 인증서 오류로 인해 개발에 불편함이 있을 수 있기 때문에 추후 HTTP 환경으로 다시 복귀시키는 것도 고려 필요.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   cacheDir: './.vite',
   resolve: {
     alias: {
@@ -20,5 +21,6 @@ export default defineConfig({
   },
   server: {
     host: true,
+    https: true,
   },
 });
