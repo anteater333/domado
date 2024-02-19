e4903ed0c03916bd42bbdac3456cf7a918af36d8
Ji-hoon Lee <anteater1056@gmail.com>
2024-02-19 16:55:26 +0900

    hotfix: Notification을 showNotification으로 변경
    프로덕션에서 환경에서 테스트 필요해 커밋함

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,47 +1,18 @@
 import { useCallback } from 'react';
 
 export const useNotification = () => {
-  if (Notification.permission !== 'granted') {
-    // Hook 처음 선언 시 권한을 물을 수 있도록 한 번 호출
-    try {
-      Notification.requestPermission().then(() => {});
-    } catch (error) {
-      if (error instanceof TypeError) {
-        // for Safari
-        Notification.requestPermission(() => {});
-      } else {
-        console.error(error);
+  const fire = useCallback(
+    async (title: string, options?: NotificationOptions) => {
+      try {
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.showNotification(title, options);
+      } catch (err) {
+        console.error('error from Notification', err);
       }
-    }
-  }
-
-  const fire = useCallback((title: string, options?: NotificationOptions) => {
-    if (!('Notification' in window)) {
-      // Notification API를 지원하지 않는 브라우저 처리
-      return;
-    }
-
-    if (Notification.permission === 'granted') {
-      new Notification(title, options);
-      return;
-    }
-
-    // Notification 권한 설정이 되어있지 않은 경우 재문의
-    try {
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') new Notification(title, options);
-      });
-    } catch (error) {
-      if (error instanceof TypeError) {
-        // for Safari
-        Notification.requestPermission((permission) => {
-          if (permission === 'granted') new Notification(title, options);
-        });
-      } else {
-        console.error(error);
-      }
-    }
-  }, []);
+    },
+    [],
+  );
 
   return { fire };
 };
