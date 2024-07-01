4e2af9cfd2a84466a29ab86021240901772272ca
anteater@mac <anteater1056@gmail.com>
2024-07-01 13:55:49 +0900

    feat - 타이머 타입 변경에 맞춰 theme-color 속성을 변경
    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff');
    
    위 코드로 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,26 @@ import { Routes, Route } from 'react-router-dom';
 import PreferenceScreen from './screens/PreferenceScreen';
 import { useRecoilValue } from 'recoil';
 import { timerTypeState } from './libs/recoil/timer';
+import { useEffect } from 'react';
 
 function App() {
   const timerType = useRecoilValue(timerTypeState);
+
+  /**
+   * 타이머 타입 변경에 맞춰 theme-color 속성을 변경
+   */
+  useEffect(() => {
+    document
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute(
+        'content',
+        timerType === 'short-break'
+          ? '#2e922c'
+          : timerType === 'long-break'
+            ? '#188bff'
+            : '#f14d30',
+      );
+  }, [timerType]);
 
   return (
     <>
